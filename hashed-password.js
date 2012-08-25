0893831b959e6a690885480c95fb8ad565301192
Daniel Hjort <daniel.hjort@gmail.com>
2012-08-25 20:28:36 +0900

    Clean up.

--- hashed-password.js
+++ hashed-password.js
@@ -12,7 +12,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 */
 
 /*
- * Bcrypt adaptation by Daniel Hjort
+ * Bcrypt adaptation by Daniel Hjort.
  * Requires the Javascript bCrypt implementation from https://code.google.com/p/javascript-bcrypt/
  */
 
@@ -33,46 +33,42 @@ function SPH_HashedPassword(password, realm, salt, rounds, bcrypt, callback) {
 
 var SPH_kPasswordPrefix = "@@";
 
-SPH_HashedPassword.prototype = {
-
-
-}
-
-	/**
-	* Fiddle with the password a bit after hashing it so that it will get through
-	* most website filters. We require one upper and lower case, one digit, and
-	* we look at the user's password to determine if there should be at least one 
-	* alphanumeric or not.
-	*/
+/**
+ * Fiddle with the password a bit after hashing it so that it will get through
+ * most website filters. We require one upper and lower case, one digit, and
+ * we look at the user's password to determine if there should be at least one 
+ * alphanumeric or not.
+ */
 function applyConstraints(hash, size, nonalphanumeric) {
+
 	var startingSize = size - 4;  // Leave room for some extra characters
 	var result = hash.substring(0, startingSize);
 	var extras = hash.substring(startingSize).split('');
 
-    // Some utility functions to keep things tidy
-    function nextExtra() { return extras.length ? extras.shift().charCodeAt(0) : 0; }
-    function nextExtraChar() { return String.fromCharCode(nextExtra()); }
-    function rotate(arr, amount) { while(amount--) arr.push(arr.shift()) }
-    function between(min, interval, offset) { return min + offset % interval; }
-    function nextBetween(base, interval) { 
-      return String.fromCharCode(between(base.charCodeAt(0), interval, nextExtra()));
-    }
-    function contains(regex) { return result.match(regex); }
+	// Some utility functions to keep things tidy
+	function nextExtra() { return extras.length ? extras.shift().charCodeAt(0) : 0; }
+	function nextExtraChar() { return String.fromCharCode(nextExtra()); }
+	function rotate(arr, amount) { while(amount--) arr.push(arr.shift()) }
+	function between(min, interval, offset) { return min + offset % interval; }
+	function nextBetween(base, interval) { 
+		return String.fromCharCode(between(base.charCodeAt(0), interval, nextExtra()));
+	}
+	function contains(regex) { return result.match(regex); }
 
-    // Add the extra characters
-    result += (contains(/[A-Z]/) ? nextExtraChar() : nextBetween('A', 26));
-    result += (contains(/[a-z]/) ? nextExtraChar() : nextBetween('a', 26));
-    result += (contains(/[0-9]/) ? nextExtraChar() : nextBetween('0', 10));
-    result += (contains(/\W/) && nonalphanumeric ? nextExtraChar() : '+');
-    while (contains(/\W/) && !nonalphanumeric) {
-      result = result.replace(/\W/, nextBetween('A', 26));
-    }
+	// Add the extra characters
+	result += (contains(/[A-Z]/) ? nextExtraChar() : nextBetween('A', 26));
+	result += (contains(/[a-z]/) ? nextExtraChar() : nextBetween('a', 26));
+	result += (contains(/[0-9]/) ? nextExtraChar() : nextBetween('0', 10));
+	result += (contains(/\W/) && nonalphanumeric ? nextExtraChar() : '+');
+	while (contains(/\W/) && !nonalphanumeric) {
+		result = result.replace(/\W/, nextBetween('A', 26));
+	}
 
-    // Rotate the result to make it harder to guess the inserted locations
-    result = result.split('');
-    rotate(result, nextExtra());
-    return result.join('');
-  }
+	// Rotate the result to make it harder to guess the inserted locations
+	result = result.split('');
+	rotate(result, nextExtra());
+	return result.join('');
+}
 
 /**
  * Callback from Bcrypt.
